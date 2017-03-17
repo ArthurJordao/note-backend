7883f618472da78183970786c49ee214fccb756e
ArthurJordao <arthurbernardijordao@gmail.com>
2017-03-17 18:47:54 -0300

    first model and commit

---
+++ src/main/java/com/notes/model/Note.java
@@ -0,0 +1,43 @@
+package com.notes.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Note {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+  @Lob
+  @Column(name="CONTENT", length=2048)
+  @NotEmpty
+  private String content;
+  private Boolean done;
+  
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public String getContent() {
+    return content;
+  }
+  public void setContent(String content) {
+    this.content = content;
+  }
+  public Boolean getDone() {
+    return done;
+  }
+  public void setDone(Boolean done) {
+    this.done = done;
+  }
+  
+}
